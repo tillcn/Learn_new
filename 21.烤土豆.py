3f658cc4d9f769582287fbb58498b170e2481b7a
lovepuyu <lovepuyu@gmail.com>
2017-09-05 09:42:11 +0800

    类

---
+++ 21.烤土豆.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# --*-- coding:utf-8 --*--
+class Potato:
+    def __init__(self):
+        self.cooklevel = 0
+        self.cookstring = "生的"
+        self.condiments = []
+    def __str__(self):
+        msg = "你的土豆处于 " + self.cookstring + " 状态"
+        if len(self.condiments)>0:
+            msg += ",添加的佐料："
+            for i in self.condiments:
+                msg = msg + i + ","
+            msg = msg.strip(",")
+        return msg
+    def cook(self,time):
+        self.cooklevel += time
+        if self.cooklevel >8:
+            self.cookstring = "烤糊的"
+        elif self.cooklevel > 5:
+            self.cookstring = "烤熟的"
+        elif self.cooklevel > 3:
+            self.cookstring = "夹生的"
+        else:
+            self.cookstring = "刚开始烤的"
+    def addcondments(self,tmp):
+        self.condiments.append(tmp)
+
+sweetPotato = Potato()
+print("------开始烤红薯------")
+print("------烤两分钟后------")
+sweetPotato.cook(2)
+print(sweetPotato)
+print("------再两分钟后------")
+sweetPotato.cook(2)
+print(sweetPotato)
+print("------添加点芥末------")
+sweetPotato.addcondments("芥末")
+print(sweetPotato)
+print("------再两分钟后------")
+sweetPotato.cook(2)
+print(sweetPotato)
