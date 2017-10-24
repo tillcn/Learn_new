d21b7b78d083dcd2c176ace379d3728ed66f5a6f
tillcn <lovepuyu@gmail.com>
2017-10-24 04:57:56 -0500

    Add files via upload

--- 猜数字_文件保存记录.py
+++ 猜数字_文件保存记录.py
@@ -11,9 +11,9 @@ for l in lines:
 score = scores.get(name)                            #查找当前玩家的数据
 if score is None:                                   #如果没找到
     score = [0,0,0]                                 #初始化数据
-game_times = int(score[0])
-min_times = int(score[1])
-total_times = int(score[2])
+game_times = int(score[0])                          #把第一个数值初始为玩了几次(程序运行过几次)
+min_times = int(score[1])                           #把第二个数值初始为最少猜出来的轮数
+total_times = int(score[2])                         #第三个数值初始为一共猜了多少回
 if game_times > 0:
     avg_times = float(total_times) / game_times
 else:
@@ -23,7 +23,7 @@ com = random.randint(1, 100)
 times = 0
 guess=True
 while guess:
-    people = input('请输入你的选择(1-100),退出请输入q，查询成绩请输入c：')
+    people = input('请输入你的选择(1-100),查询成绩请输入c：')
     times += 1
     if people.isdigit() and 0 < int(people) < 101:
         peo = int(people)
@@ -34,22 +34,18 @@ while guess:
             print('你输入的数字太大。')
         elif peo < com:
             print('你输入数字太小')
-    elif people == 'q' or people == 'Q':
-        break
     elif people == 'c' or people == 'C':
-        print('pass')
+        print('%s,你已经玩了%d次,最少%d轮猜出答案,平均%.2f猜出答案' % (name,game_times,min_times,avg_times))
     else:
         print('你的输入有误，请重新输入')
 if game_times == 0 or times < min_times:
     min_times = times
-total_times += times
-game_times += 1
-scores[name] = [str(game_times),str(min_times),str(total_times)]
-result = ''
-for n in scores:
-    line = n+' ' + ' '.join(scores[n]) + '\n'
+total_times += times                                                            #猜的部次数
+game_times += 1                                                                 #玩了多少回
+scores[name] = [str(game_times),str(min_times),str(total_times)]                #把上面的数值以name为key名存为value
+result = ''                                                                     #定义一个空字段
+for n in scores:                                                                #for循环字典n得到的是每一项的key, scores[n]就是对应的value
+    line = n+' ' + ' '.join(scores[n]) + '\n'                                   #n就key，scores是值     .join 列表转字符串                              
     result += line
 with open('Score.txt','w') as f:
     f.write(result)
-
-
